b0eb57b827e91713fae8baec6eb66b32deeb9a70
panjiprann <panjipranantya20@gmail.com>
2024-05-11 12:41:16 +0700

    menyelesaikan int push

--- p.cpp
+++ p.cpp
@@ -29,6 +29,7 @@ class Stack {
         newNode->next = top; // 3. set the next pointer of the new node to the current top node
         top = newNode; // 4. Update the top pointer to the new node
         cout << "Push value: " << value << endl;
+        return value;
     } 
 
 };
