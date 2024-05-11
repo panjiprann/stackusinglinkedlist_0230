6d41fcfd5511a90bfd554bd8c175a06779a91b71
panjiprann <panjipranantya20@gmail.com>
2024-05-11 12:57:45 +0700

    memasukkan fungsi kedalam void peek

--- p.cpp
+++ p.cpp
@@ -43,7 +43,16 @@ class Stack {
 
     // Peek/Top operation: Retrieve the value of the topmost element without removing it
     void peek() {
-        
+        if (top == NULL) {
+            cout << "List is empty." << endl;
+        }
+        else {
+            Node* current = top;
+            while (current != NULL) {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }        
+        }  
     }
 
 };
