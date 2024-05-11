d4f43333e187d92c9df00ec4020c2af03c6ab5c3
panjiprann <panjipranantya20@gmail.com>
2024-05-11 12:08:28 +0700

    menyelesaikan class node

--- p.cpp
+++ p.cpp
@@ -4,6 +4,12 @@ using namespace std;
 //node class representing a single node in the linked list
 class Node {
     public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
 };
 
 
