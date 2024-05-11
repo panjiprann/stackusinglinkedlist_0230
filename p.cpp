fefd73e729950aff73e260ea6c6bd09ce23a4b04
panjiprann <panjipranantya20@gmail.com>
2024-05-11 13:17:05 +0700

    menyelesaikan int main

--- p.cpp
+++ p.cpp
@@ -63,8 +63,53 @@ class Stack {
 
 };
 
-
-
 int main(){
+    Stack stack;
 
-};
+    int choice = 0;
+    int value;
+
+    while (choice != 5){
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter the value to push: ";
+                cin >> value;
+                stack.push(value); // Push the entered value onto the stack
+                break;
+            case 2:
+            if (!stack.isEmpty()) {
+                stack.pop(); // Pop the topmost element from the stack
+            }
+            else {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+        case 3:
+            if (!stack.isEmpty()) {
+                stack.peek(); // Get the value of the top element
+            }
+            else {
+                cout << "Stack is empty. No top value." << endl;
+            }
+            break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+
+}
